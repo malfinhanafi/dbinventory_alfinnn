249f928e10219205e9b5b57e0cf1b8a9001eed63
malfinhanafi <finmarhas@gmail.com>
2025-02-20 14:11:10 +0700

    aamiinn

---
+++ database/dbinventory_alfinnnnnnn.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 20, 2025 at 05:29 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbinventory_alfinnnnnnn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` varchar(20) NOT NULL,
+  `nama_barang` varchar(30) DEFAULT NULL,
+  `id_jenis` varchar(20) DEFAULT NULL,
+  `stok` int DEFAULT NULL,
+  `harga` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_jenis`, `stok`, `harga`) VALUES
+('A1111', 'iPhone Mini', 'B112', 22, 1400000),
+('A2112', 'iPhone 14', 'B111', 50, 20000000),
+('A2113', 'iPhone Mini', 'B112', 20, 60000000),
+('A2114', 'iPhone 12', 'B113', 10, 11000000),
+('A2115', 'iPhone 15', 'B114', 5, 25000000),
+('A2116', 'iPhone 16', 'B115', 3, 25000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_barang`
+--
+
+CREATE TABLE `jenis_barang` (
+  `id_jenis` varchar(20) NOT NULL,
+  `nama_jenis` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `jenis_barang`
+--
+
+INSERT INTO `jenis_barang` (`id_jenis`, `nama_jenis`) VALUES
+('B111', 'ProMax'),
+('B112', 'Pro'),
+('B113', 'Pro'),
+('B114', 'ProMax'),
+('B115', 'ProMax');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_jenis` (`id_jenis`);
+
+--
+-- Indexes for table `jenis_barang`
+--
+ALTER TABLE `jenis_barang`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_barang` (`id_jenis`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
